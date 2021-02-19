8607684acab8a4d6e61b302c7e899b08e03fb234
mike <mikeadouglass@gmail.com>
2021-02-19 13:01:04 -0500

    More modules - indexing + add ical4j

---
+++ bw-wfmodules-rw/src/main/java/org/bedework/wfmodules/WfModulesRw.java
@@ -0,0 +1,10 @@
+/* ********************************************************************
+    Appropriate copyright notice
+*/
+package org.bedework.wfmodules;
+
+/**
+ * User: mike Date: 2/15/21 Time: 17:16
+ */
+public class WfModulesRw {
+}
